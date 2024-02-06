6316027d969f48fb7a63607dc215f176784a6c7e
rsmajumder <92791105+rsmajumder@users.noreply.github.com>
2024-02-06 20:49:41 +0000

    initial commit

--- web/web/settings.py
+++ web/web/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- web/web/urls.py
+++ web/web/urls.py
@@ -18,10 +18,13 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('shop.urls')),
+     path('', RedirectView.as_view(url='shop/', permanent=False)),
+    path('shop/', include('shop.urls')),
+
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
